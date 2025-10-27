c3888fd7ef25feae31bf2bcfc5a7d0c2625dc748
SSu4K <ssudak.kontakt@gmail.com>
2025-10-27 10:46:44 +0100

    Updated translation files.

---
+++ translations/MCAS_en.ts
@@ -0,0 +1,101 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../source/mainwindow.cpp" line="35"/>
+        <source>&amp;Tools</source>
+        <translation>&amp;Tools</translation>
+    </message>
+    <message>
+        <location filename="../source/mainwindow.cpp" line="36"/>
+        <source>Open Microcode Editor</source>
+        <translation>Open Microcode Editor</translation>
+    </message>
+    <message>
+        <location filename="../source/mainwindow.cpp" line="43"/>
+        <source>&amp;View</source>
+        <translation>View</translation>
+    </message>
+    <message>
+        <location filename="../source/mainwindow.cpp" line="44"/>
+        <source>Theme</source>
+        <translation>Theme</translation>
+    </message>
+    <message>
+        <location filename="../source/mainwindow.cpp" line="46"/>
+        <source>System Default</source>
+        <translation>System Default</translation>
+    </message>
+    <message>
+        <location filename="../source/mainwindow.cpp" line="47"/>
+        <source>Light</source>
+        <translation>Light</translation>
+    </message>
+    <message>
+        <location filename="../source/mainwindow.cpp" line="48"/>
+        <source>Dark</source>
+        <translation>Dark</translation>
+    </message>
+</context>
+<context>
+    <name>MicrocodeEditor::MicrocodeEditorWindow</name>
+    <message>
+        <location filename="../source/MicrocodeEditor/microcodeeditorwindow.cpp" line="18"/>
+        <source>Microcode</source>
+        <translation>Microcode</translation>
+    </message>
+    <message>
+        <location filename="../source/MicrocodeEditor/microcodeeditorwindow.cpp" line="19"/>
+        <source>Jump Tables</source>
+        <translation>Jump Tables</translation>
+    </message>
+    <message>
+        <location filename="../source/MicrocodeEditor/microcodeeditorwindow.cpp" line="31"/>
+        <source>&amp;File</source>
+        <translation>&amp;File</translation>
+    </message>
+    <message>
+        <location filename="../source/MicrocodeEditor/microcodeeditorwindow.cpp" line="33"/>
+        <source>&amp;New</source>
+        <translation>&amp;New</translation>
+    </message>
+    <message>
+        <location filename="../source/MicrocodeEditor/microcodeeditorwindow.cpp" line="34"/>
+        <source>&amp;Open...</source>
+        <translation>&amp;Open</translation>
+    </message>
+    <message>
+        <location filename="../source/MicrocodeEditor/microcodeeditorwindow.cpp" line="38"/>
+        <source>&amp;Save</source>
+        <translation>&amp;Save</translation>
+    </message>
+    <message>
+        <location filename="../source/MicrocodeEditor/microcodeeditorwindow.cpp" line="39"/>
+        <source>Save &amp;As...</source>
+        <translation>Save &amp;As...</translation>
+    </message>
+    <message>
+        <location filename="../source/MicrocodeEditor/microcodeeditorwindow.cpp" line="43"/>
+        <source>E&amp;xit</source>
+        <translation>E&amp;xit</translation>
+    </message>
+    <message>
+        <location filename="../source/MicrocodeEditor/microcodeeditorwindow.cpp" line="62"/>
+        <source>Open Microcode File</source>
+        <translation>Open Microcode File</translation>
+    </message>
+    <message>
+        <location filename="../source/MicrocodeEditor/microcodeeditorwindow.cpp" line="63"/>
+        <location filename="../source/MicrocodeEditor/microcodeeditorwindow.cpp" line="112"/>
+        <source>Microcode Files (*.txt *.mc *.tsv)</source>
+        <translation>Microcode Files (*.txt *.mc *.tsv)</translation>
+    </message>
+    <message>
+        <location filename="../source/MicrocodeEditor/microcodeeditorwindow.cpp" line="110"/>
+        <source>Save Microcode File As</source>
+        <translation>Save Microcode File As</translation>
+    </message>
+</context>
+</TS>
---
+++ translations/MCAS_pl.ts
@@ -0,0 +1,101 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pl_PL">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../source/mainwindow.cpp" line="35"/>
+        <source>&amp;Tools</source>
+        <translation>&amp;Narzędzia</translation>
+    </message>
+    <message>
+        <location filename="../source/mainwindow.cpp" line="36"/>
+        <source>Open Microcode Editor</source>
+        <translation>Edytor Mikrokodu</translation>
+    </message>
+    <message>
+        <location filename="../source/mainwindow.cpp" line="43"/>
+        <source>&amp;View</source>
+        <translation>&amp;Widok</translation>
+    </message>
+    <message>
+        <location filename="../source/mainwindow.cpp" line="44"/>
+        <source>Theme</source>
+        <translation>Motyw</translation>
+    </message>
+    <message>
+        <location filename="../source/mainwindow.cpp" line="46"/>
+        <source>System Default</source>
+        <translation>Systemowy</translation>
+    </message>
+    <message>
+        <location filename="../source/mainwindow.cpp" line="47"/>
+        <source>Light</source>
+        <translation>Jasny</translation>
+    </message>
+    <message>
+        <location filename="../source/mainwindow.cpp" line="48"/>
+        <source>Dark</source>
+        <translation>Ciemny</translation>
+    </message>
+</context>
+<context>
+    <name>MicrocodeEditor::MicrocodeEditorWindow</name>
+    <message>
+        <location filename="../source/MicrocodeEditor/microcodeeditorwindow.cpp" line="18"/>
+        <source>Microcode</source>
+        <translation>Mikrokod</translation>
+    </message>
+    <message>
+        <location filename="../source/MicrocodeEditor/microcodeeditorwindow.cpp" line="19"/>
+        <source>Jump Tables</source>
+        <translation>Tablice skoków</translation>
+    </message>
+    <message>
+        <location filename="../source/MicrocodeEditor/microcodeeditorwindow.cpp" line="31"/>
+        <source>&amp;File</source>
+        <translation>&amp;Plik</translation>
+    </message>
+    <message>
+        <location filename="../source/MicrocodeEditor/microcodeeditorwindow.cpp" line="33"/>
+        <source>&amp;New</source>
+        <translation>&amp;Nowy</translation>
+    </message>
+    <message>
+        <location filename="../source/MicrocodeEditor/microcodeeditorwindow.cpp" line="34"/>
+        <source>&amp;Open...</source>
+        <translation>&amp;Otwórz...</translation>
+    </message>
+    <message>
+        <location filename="../source/MicrocodeEditor/microcodeeditorwindow.cpp" line="38"/>
+        <source>&amp;Save</source>
+        <translation>&amp;Zapisz</translation>
+    </message>
+    <message>
+        <location filename="../source/MicrocodeEditor/microcodeeditorwindow.cpp" line="39"/>
+        <source>Save &amp;As...</source>
+        <translation>Zapisz &amp;Jako...</translation>
+    </message>
+    <message>
+        <location filename="../source/MicrocodeEditor/microcodeeditorwindow.cpp" line="43"/>
+        <source>E&amp;xit</source>
+        <translation>Z&amp;amknij</translation>
+    </message>
+    <message>
+        <location filename="../source/MicrocodeEditor/microcodeeditorwindow.cpp" line="62"/>
+        <source>Open Microcode File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../source/MicrocodeEditor/microcodeeditorwindow.cpp" line="63"/>
+        <location filename="../source/MicrocodeEditor/microcodeeditorwindow.cpp" line="112"/>
+        <source>Microcode Files (*.txt *.mc *.tsv)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../source/MicrocodeEditor/microcodeeditorwindow.cpp" line="110"/>
+        <source>Save Microcode File As</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
